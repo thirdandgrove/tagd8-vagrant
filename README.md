a7405a0a1ce4193b7b556a46b7f3d8b572a23a9e
James Watts <jamescwatts@gmail.com>
2016-01-21 16:13:56 -0500

    Create stand alone Vagrant VM / provision, and update README with usage.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # tagd8-vagrant
-Drupal 8 Vagrant / Puppet Development VM Config
+
+A [Vagrant](https://www.vagrantup.com/) virtual machine configuration and provisioner,
+for use as a test or development environment for Drupal applications.
+
+**NOTE** this virtual machine should not be used in a production environment.
+
+## Usage
+The Vagrant configuration and [Puppet](https://puppetlabs.com/) provisioning scripts
+are contained in the `/private` directory. Using the VM can be accomplished via
+the following steps:
+
+* Install [Vagrant](https://www.vagrantup.com/).
+* Copy the entire `/private` directory to the root directory of your Drupal codebase.
+* If desired, edit the `/private/Vagrantfile` and set your preferred local hostname and IP address (lines 15 and 16).
+* From the `/private` directory, run `vagrant up`. This will create and provision the VM.
+* Add a new line to your `/etc/hosts` file for the new local URL / IP: `192.168.10.10 local.example.com`.
+* Create or load your site database into the VM `mysql` server. For testing purposes the provisioner creates a user and database named `vagrant`.
+* Connect to your new test site in your browser at the URL you've chosen.
+* Your site code is mounted on the Vagrant VM in the `/vagrant` directory.
---
+++ private/puppet_manifests/default.pp
@@ -0,0 +1,225 @@
+# Puppet Stages
+stage {
+    'users':       before => Stage['folders'];
+    'folders':     before => Stage['updates'];
+    'updates':     before => Stage['packages'];
+    'packages':    before => Stage['apache'];
+    'apache':      before => Stage['mysql'];
+    'mysql':       before => Stage['php'];
+    'php':         before => Stage['composer'];
+    'composer':    before => Stage['drush'];
+    'drush':       before => Stage['drupal'];
+    'drupal':      before => Stage['solr'];
+    'solr':        before => Stage['services'];
+    'services':    before => Stage['main'];
+}
+
+class users {
+    group { "www-data":
+        ensure => "present",
+     }
+}
+
+class folders {
+    file { ['/var/www']:
+        ensure => 'directory',
+        owner => 'www-data',
+        group => 'www-data',
+        mode => 0755
+    }
+}
+
+class updates {
+    exec {
+        "aptitude-update":
+            command => "/usr/bin/aptitude update -y -q",
+            timeout => 0;
+    }
+}
+
+class packages {
+    package {[
+            "git",
+            "ntp",
+            "curl",
+            "unzip",
+            "apache2",
+            "imagemagick",
+            "sendmail",
+            "memcached",
+            "default-jre",
+            "mysql-server",
+            "build-essential",
+            "libcurl3-openssl-dev",
+            "php5",
+            "php5-gd",
+            "php5-cli",
+            "php5-dev",
+            "php5-apcu",
+            "php5-curl",
+            "php5-mysql",
+            "php5-mcrypt",
+            "php5-memcache",
+            "php-pear",
+            "tomcat6",
+            ]:
+        ensure => "present",
+    }
+}
+
+class apache {
+    $location = "Cambridge"
+    $country = "US"
+    $state = "MA"
+    $organization = "TAG"
+    $unit = "Search"
+    $commonname = "www.example.com"
+    $keyname = "www_example_com"
+
+    $subject = "/C=${country}/ST=${state}/L=${location}/O=${organization}/OU=${unit}/CN=${commonname}"
+    exec {
+        "clear-apache-conf":
+            command => '/usr/bin/sudo rm /etc/apache2/sites-enabled/*default*',
+            onlyif => '/bin/ls /etc/apache2/sites-enabled/000-default.conf';
+
+        "copy-apache-conf":
+            command => '/usr/bin/sudo cp /vagrant/private/vagrant/apache.conf /etc/apache2/sites-enabled/apache.conf';
+
+        "copy-apache-ssl-conf":
+            command => '/usr/bin/sudo cp /vagrant/private/vagrant/apache_ssl.conf /etc/apache2/sites-enabled/apache_ssl.conf';
+
+        "apache-rewrite":
+            command => '/usr/bin/sudo a2enmod rewrite';
+
+        "apache-ssl-mod":
+            command => '/usr/bin/sudo a2enmod ssl';
+
+        "openssl-csr":
+            command => "/usr/bin/openssl req -new -newkey rsa:2048 -x509 -days 365 -nodes -out ${keyname}.crt -keyout ${keyname}.key -subj \"${subject}\"",
+            cwd => '/home/vagrant',
+            creates => "/home/vagrant/${keyname}.key";
+    }
+}
+
+class mysql {
+    exec {
+        "mysql-bind-address":
+            command => '/usr/bin/sudo cp /vagrant/private/vagrant/bind.cnf /etc/mysql/conf.d/bind.cnf',
+            unless => '/bin/ls /etc/mysql/conf.d/bind.cnf';
+
+        "mysql-privilege":
+            command => '/usr/bin/mysql -uroot -h 127.0.0.1 -e \'GRANT ALL PRIVILEGES ON *.* TO "vagrant"@"%" IDENTIFIED BY "vagrant"; FLUSH PRIVILEGES;\'';
+
+        "mysql-create-database":
+            command => '/usr/bin/mysql -uvagrant -pvagrant -h 127.0.0.1 -e \'CREATE DATABASE vagrant;\'',
+            unless => '/usr/bin/mysql -uroot -h 127.0.0.1 vagrant';
+    }
+}
+
+class php {
+    exec {
+        "enable-php-mcrypt":
+            command => '/usr/bin/sudo php5enmod mcrypt';
+        "php-apache2-apc":
+            command => '/bin/echo "apc.rfc1867 = 1" >> /etc/php5/apache2/php.ini';
+        "php-cli-apc":
+            command => '/bin/echo "apc.rfc1867 = 1" >> /etc/php5/cli/php.ini';
+
+        "php-pecl-upload":
+            command => '/usr/bin/sudo pecl install -Z uploadprogress',
+            unless => '/bin/ls /usr/share/doc/php5/PEAR/uploadprogress';
+        "php-ini-progress":
+            command => '/bin/echo "extension=uploadprogress.so" >> /etc/php5/apache2/php.ini',
+            require => Exec['php-pecl-upload'];
+
+        "memcached-bind-address":
+            command => '/usr/bin/sudo cp /vagrant/private/vagrant/memcached.conf /etc/memcached.conf';
+    }
+}
+
+class composer {
+    exec {
+        "composer-download":
+            command => '/usr/bin/curl -sS https://getcomposer.org/installer | /usr/bin/sudo /usr/bin/php -- --install-dir=/usr/local/bin --filename=composer',
+            unless => '/bin/ls /usr/share/php/composer';
+    }
+
+    # Scripts in /etc/profile.d/ will be executed on user login when /etc/profile is source(d)
+    file {
+        "/etc/profile.d/composer.sh":
+            content => 'export PATH="~/.composer/vendor/bin:$PATH"'
+    }
+}
+
+class drush {
+    exec {
+
+        "drush-install":
+            environment => ["COMPOSER_HOME=/home/vagrant/.composer"],
+             command => '/usr/local/bin/composer global require drush/drush:dev-master',
+            unless => '/bin/ls /usr/share/php/drush';
+
+        "console-table-install":
+            environment => ["COMPOSER_HOME=/home/vagrant/.composer"],
+            command => '/usr/local/bin/composer global require pear/console_table:1.2.1';
+    }
+}
+
+class drupal {
+    exec {
+        "local-settings":
+            command => '/bin/cp /vagrant/sites/example.settings.local.php /vagrant/sites/default/settings.local.php',
+            onlyif => '/bin/ls /vagrant/sites/example.settings.local.php',
+            unless => '/bin/ls /vagrant/sites/default/settings.local.php';
+    }
+}
+
+class solr {
+    exec {
+        "download":
+            command => '/usr/bin/wget http://archive.apache.org/dist/lucene/solr/3.5.0/apache-solr-3.5.0.tgz';
+        "untar":
+            command => '/bin/tar -zxvf apache-solr-3.5.0.tgz',
+            require => Exec['download'];
+        "locate":
+            command => '/bin/mv apache-solr-3.5.0 /usr/share/solr',
+            require => Exec['untar'];
+        "config":
+            command => '/bin/cp /vagrant/private/vagrant/solr.xml /etc/tomcat6/Catalina/localhost/',
+            require => Exec['locate'];
+        "own":
+            command => '/bin/chown -R tomcat6 /usr/share/solr/example/solr',
+            require => Exec['config'];
+        "own_xml":
+            command => '/bin/chown tomcat6 /etc/tomcat6/Catalina/localhost/solr.xml',
+            require => Exec['own'];
+    }
+}
+
+class services {
+    exec {
+        "apache-restart":
+            command => '/usr/bin/sudo service apache2 restart';
+        "memcached-restart":
+            command => '/usr/bin/sudo /etc/init.d/memcached restart';
+        "mysql-restart":
+            command => '/usr/bin/sudo service mysql restart';
+        "tomcat-restart":
+            command => '/usr/bin/sudo /etc/init.d/tomcat6 restart';
+    }
+}
+
+class {
+    users:       stage => "users";
+    folders:     stage => "folders";
+    updates:     stage => "updates";
+    packages:    stage => "packages";
+    apache:      stage => "apache";
+    mysql:       stage => "mysql";
+    php:         stage => "php";
+    composer:    stage => "composer";
+    drush:       stage => "drush";
+    drupal:      stage => "drupal";
+    solr:        stage => "solr";
+    services:    stage => "services";
+}
